2c04585266c52dad21c5c1754dbe8e28d591aba8
IsmaelCardoso <ismael.cardosos@gmail.com>
2022-04-25 19:42:55 -0300

    chore(SSG - Static Site Generation):  - change SSR to SSG (getStaticProps)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import stripe from "../services/stripe";
@@ -41,8 +41,18 @@ const Home = ({ product }: IHomeProps) => {
     </>
   );
 };
+/*
+ * Requests:
+ * Client-side (traditional)
+ * Server-side SSR - GetServerSideProps
+ * Static side SSG - getStaticProps (+ revalidate)
+ *
+ * Ex:
+ * Blog post -> SSG
+ * Blog comment -> Client-side
+ **/
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KsUrDBiYgsIPADWl6yDXdm7", {
     expand: ["product"],
   });
@@ -59,6 +69,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
 
